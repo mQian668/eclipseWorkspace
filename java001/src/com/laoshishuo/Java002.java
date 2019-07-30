dd4f78f4b3a318716b4d221f518139a109c77bb8
mQian668 <306002363@qq.com>
2019-07-30 11:18:36 +0800

    do something change!

--- java001/src/com/laoshishuo/Java002.java
+++ java001/src/com/laoshishuo/Java002.java
@@ -1,8 +1,11 @@
 package com.laoshishuo;
 
+import java.util.Date;
+
 public class Java002 {
 	public static void main(String[] args) {
 		System.out.println("change to lower case");
 		System.out.println("now is dev branch");
+		System.out.println(new Date());
 	}
 }
