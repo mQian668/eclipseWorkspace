f85d5211259e0bff0c302ce8eb59c9f0756a7f8b
mQian668 <306002363@qq.com>
2019-07-30 10:24:52 +0800

    now is dev branch

--- java001/src/com/laoshishuo/Java002.java
+++ java001/src/com/laoshishuo/Java002.java
@@ -3,5 +3,6 @@ package com.laoshishuo;
 public class Java002 {
 	public static void main(String[] args) {
 		System.out.println("change to lower case");
+		System.out.println("now is dev branch");
 	}
 }
